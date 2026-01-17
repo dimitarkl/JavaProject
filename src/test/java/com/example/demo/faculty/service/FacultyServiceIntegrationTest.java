381289a40051bd0945736ffec7783023afdf165d
VSMihaylova <viktoria.s.mihaylova@gmail.com>
2026-01-17 16:14:10 +0200

    Fixed integration tests isolation

--- src/test/java/com/example/demo/faculty/service/FacultyServiceIntegrationTest.java
+++ src/test/java/com/example/demo/faculty/service/FacultyServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Transactional
 @DisplayName("FacultyService Integration Tests")
 class FacultyServiceIntegrationTest {
